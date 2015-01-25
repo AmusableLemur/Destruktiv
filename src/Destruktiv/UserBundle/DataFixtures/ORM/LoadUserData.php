1033077cb6048dd6d92e9b58667f2935e38b521c
Rasmus Larsson <rasmus@rasmuslarsson.se>
2015-01-25 22:47:28 +0100

    Add admin fixture

---
+++ src/Destruktiv/UserBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Destruktiv\UserBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class LoadUserData implements FixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $userManager = $this->container->get('fos_user.user_manager');
+
+        $user = $userManager->createUser();
+        $user->setUsername('admin');
+        $user->setEmail('admin@destruktiv.se');
+        $user->setPlainPassword('password');
+        $user->setEnabled(true);
+        $user->setRoles(array('ROLE_ADMIN'));
+
+        $userManager->updateUser($user, true);
+    }
+}
+
