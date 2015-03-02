f446f05c05c7170a3c9af28c5e7618eee6cdbb6b
Rasmus Larsson <rasmus@rasmuslarsson.se>
2015-03-02 11:04:37 +0100

    Fix problem with nullable name

--- src/Destruktiv/UserBundle/Entity/User.php
+++ src/Destruktiv/UserBundle/Entity/User.php
@@ -26,7 +26,7 @@ class User extends BaseUser
     /**
      * @var string
      *
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     protected $name;
 
