66ff049fa99689967474e7750bfa4041cf6f8ea3
Rasmus Larsson <rasmus@rasmuslarsson.se>
2015-05-14 22:00:26 +0000

    Implement game logic

--- src/Destruktiv/GameBundle/Controller/VaultController.php
+++ src/Destruktiv/GameBundle/Controller/VaultController.php
@@ -14,13 +14,77 @@ class VaultController extends Controller
      * @Route("/vault", name="vault")
      * @Template()
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        // Redirect to appropriate level
+        $level = $this->getUser()->getVaultLevel();
+
+        if ($level === null) {
+            $em = $this->getDoctrine()->getManager();
+            $query = $em->createQuery('SELECT v FROM DestruktivGameBundle:VaultLevel v ORDER BY v.level')
+                ->setMaxResults(1);
+            $level = $query->getOneOrNullResult();
+        }
+
+        $form = $this->createLevelForm($level);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $data = $form->getData();
+
+            if (strtolower($level->getPassword()) == strtolower($data["password"])) {
+                $query = $em->createQuery(
+                        'SELECT v
+                        FROM DestruktivGameBundle:VaultLevel v
+                        WHERE v.level > :level
+                        ORDER BY v.level'
+                    )
+                    ->setParameter("level", $level->getLevel())
+                    ->setMaxResults(1);
+                $nextLevel = $query->getOneOrNullResult();
+
+                if ($nextLevel === null) {
+                    return [
+                        "page" => "games",
+                        "level" => null,
+                        "form" => $form->createView()
+                    ];
+                }
+
+                $user = $this->getUser();
+
+                $user->setVaultLevel($nextLevel);
+                $this->get("fos_user.user_manager")->updateUser($user);
+
+                return $this->redirect(
+                    $this->generateUrl("vault")
+                );
+            }
+        }
 
         return [
-            "page" => "games"
+            "page" => "games",
+            "level" => $level,
+            "form" => $form->createView()
         ];
+    }
+
+    private function createLevelForm(VaultLevel $level)
+    {
+        return $this->createFormBuilder()
+            ->add("password", "text", [
+                "label" => false,
+                "required" => false,
+                "attr" => [
+                    "autofocus" => true
+                ]
+            ])
+            ->add("next", "submit", [
+                "label" => "Nästa nivå",
+                "attr" => [
+                    "class" => "btn-primary pull-right"
+                ]
+            ])
+            ->getForm();
     }
 
     /**
@@ -76,7 +140,8 @@ class VaultController extends Controller
         ];
     }
 
-    private function createAdminForm(VaultLevel $level) {
+    private function createAdminForm(VaultLevel $level)
+    {
         return $this->createFormBuilder($level)
             ->add("level", "integer", [
                 "label" => "Nivå"
