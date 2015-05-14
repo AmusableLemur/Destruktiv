48fdfeeaa8f98e8a304cfd1332dd8d2a2a649c77
Rasmus Larsson <rasmus@rasmuslarsson.se>
2015-05-14 17:35:49 +0000

    Create basic admin interface

--- src/Destruktiv/GameBundle/Controller/VaultController.php
+++ src/Destruktiv/GameBundle/Controller/VaultController.php
@@ -2,6 +2,7 @@
 
 namespace Destruktiv\GameBundle\Controller;
 
+use Destruktiv\GameBundle\Entity\VaultLevel;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -17,5 +18,50 @@ class VaultController extends Controller
         return [
             "page" => "games"
         ];
+    }
+
+    /**
+     * @Route("/vault/admin", name="vault_admin")
+     * @Template()
+     */
+    public function adminAction()
+    {
+        $levels = [];
+
+        return [
+            "page" => "games",
+            "levels" => $levels
+        ];
+    }
+
+    /**
+     * @Route("/vault/admin/{id}", name="vault_edit")
+     * @Template()
+     */
+    public function editAction()
+    {
+    }
+
+    /**
+     * @Route("/vault/admin/new", name="vault_new")
+     * @Template()
+     */
+    public function createAction()
+    {
+    }
+
+    /**
+     * @Route("/vault/admin/{id}/delete", name="vault_delete")
+     */
+    public function deleteAction()
+    {
+    }
+
+    private function getAdminForm(VaultLevel $level) {
+        return $this->createFormBuilder($level)
+            ->add("level")
+            ->add("hint")
+            ->add("password")
+            ->getForm();
     }
 }
--- src/Destruktiv/GameBundle/Entity/VaultLevel.php
+++ src/Destruktiv/GameBundle/Entity/VaultLevel.php
@@ -21,6 +21,21 @@ class VaultLevel
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $level;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $hint;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $password;
+
 
     /**
      * Get id
@@ -31,5 +46,76 @@ class VaultLevel
     {
         return $this->id;
     }
-}
 
+    /**
+     * Set level
+     *
+     * @param integer $level
+     *
+     * @return VaultLevel
+     */
+    public function setLevel($level)
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    /**
+     * Get level
+     *
+     * @return integer
+     */
+    public function getLevel()
+    {
+        return $this->level;
+    }
+
+    /**
+     * Set hint
+     *
+     * @param string $hint
+     *
+     * @return VaultLevel
+     */
+    public function setHint($hint)
+    {
+        $this->hint = $hint;
+
+        return $this;
+    }
+
+    /**
+     * Get hint
+     *
+     * @return string
+     */
+    public function getHint()
+    {
+        return $this->hint;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return VaultLevel
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+}
