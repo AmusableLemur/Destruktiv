1a5021860edd547c3249d5136a0709e65352c951
Rasmus Larsson <rasmus@rasmuslarsson.se>
2015-05-14 08:18:13 +0000

    Update nav to include game menu

--- src/Destruktiv/GameBundle/Controller/VaultController.php
+++ src/Destruktiv/GameBundle/Controller/VaultController.php
@@ -9,11 +9,13 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class VaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/vault", name="vault")
      * @Template()
      */
     public function indexAction()
     {
-        return [];
+        return [
+            "page" => "games"
+        ];
     }
 }
