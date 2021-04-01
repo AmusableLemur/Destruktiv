452b099d5d2df129d40b105a89778ad9f6f01504
Rasmus Larsson <rasmus@rasmuslarsson.se>
2021-04-01 23:42:11 +0200

    fix login form

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
